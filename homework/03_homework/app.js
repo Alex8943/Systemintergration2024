6dc7eb5bf363c101d8668b70c61a7dc4b55e5431
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-02-27 13:09:33 +0100

    03 SSE opgave

---
+++ homework/03_homework/app.js
@@ -0,0 +1,37 @@
+// app.js
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/sse', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    
+    let count = 0;
+    const intervalId = setInterval(() => {
+        if(count <= 5){
+            res.write(`data: Message ${count}\n\n`);
+            count++;
+        }else{
+            clearInterval(intervalId);
+            res.write('data: Connnection is closed\n\n');
+            res.end();
+        }
+        
+    }, 1000);
+
+
+    req.on('close', () => {
+        clearInterval(intervalId);
+    });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on localhost:${PORT}`);
+});
