7cf1c57ee243a0c552f24994edad0be447ce8c84
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-04-12 16:56:21 +0200

    websockets mm.

---
+++ homework/07_homework/app.js
@@ -0,0 +1,21 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received message: %s', data);
+
+    // Broadcast the received message to all clients except the sender
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocketServer.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+
+  // Send initial message to the client upon connection
+  ws.send('Connected to the server!');
+});
