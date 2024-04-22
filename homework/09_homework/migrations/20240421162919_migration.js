cce6089e32d5c4c9cb25e2f47ede6d002a1b0662
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-04-22 21:55:56 +0200

    working in progress, mirgrations does not work

--- homework/09_homework/migrations/20240421162919_migration.js
+++ homework/09_homework/migrations/20240421162919_migration.js
@@ -1,15 +1,16 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
+exports.up = async function(knex) {
+    const hasUsersTable = await knex.schema.hasTable('users');
+    if (!hasUsersTable) {
+      await knex.schema.createTable('users', function(table) {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('lastname').notNullable();
+        table.timestamps(true, true);
+      });
+    }
+  };
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+  exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('users');
+  };
   
-};
