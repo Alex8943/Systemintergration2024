6585d8d0f0d3ccab5ca83051e2650ab8f39e9278
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2024-04-23 12:09:11 +0200

    cleaned out index.js

--- homework/09_homework/index.js
+++ homework/09_homework/index.js
@@ -6,22 +6,19 @@ const Knex = require('knex');
 
 async function testDBConnection() {
 
-    // Set up Knex and Objection.js with the database configuration from knexfile.js
     const knex = Knex(knexfile.development);
     Model.knex(knex);
 
     try {
-    
-        const countries = await Countries.query();
-
-        const users = await Users.query();
+        await Countries.query();
+        await Users.query();
     
         console.log('Both database connections are successful.');
 
     } catch (error) {
         console.error('Connection failed:', error);
     } finally {
-        // Destroy connection
+   
         await knex.destroy();
     }
 }
